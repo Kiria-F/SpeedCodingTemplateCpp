54b4e27217df8d109f122d2399bf96ff4116ac7b
Sedo-KFM <fedor122448@gmail.com>
2023-11-16 12:19:40 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <utility>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <stack>
+#include <random>
+
+using namespace std;
+
+namespace my {
+
+    typedef unsigned int uint;
+    typedef long long ll;
+    typedef unsigned long long ull;
+
+    template<typename T>
+    string to_string(const vector<T>& input) {
+        std::stringstream ss;
+        ss << "[";
+        if (input.size() > 0) {
+            for (auto it = input.cbegin(); it < --input.cend(); ++it)
+                ss << *it << ", ";
+            ss << *--input.end();
+        }
+        ss << "]";
+        return ss.str();
+    }
+
+    template<typename T>
+    ostream& operator<<(ostream& os, const vector<T>& input) {
+        os << to_string<T>(input);
+        return os;
+    }
+
+    ll bin_power(ll a, ll b, ll mod) {
+        ll res = 1;
+        while (b) {
+            if (b & 1) {
+                res = res * a % mod;
+            }
+            a = a * a % mod;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    ll bin_power(ll a, ll b) {
+        ll res = 1;
+        while (b) {
+            if (b & 1) {
+                res = res * a;
+            }
+            a = a * a;
+            b >>= 1;
+        }
+        return res;
+    }
+}
+
+using namespace my;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
