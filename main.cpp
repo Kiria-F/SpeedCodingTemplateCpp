9bf1818b75d5652659bc9c94b144f7e8dd127e92
Sedo-KFM <fedor122448@gmail.com>
2023-12-23 12:27:03 +0400

    Add new cin inits

--- main.cpp
+++ main.cpp
@@ -15,6 +15,17 @@
 #include <unordered_set>
 #include <unordered_map>
 
+// #define DEBUG
+#ifdef DEBUG
+#define CONTAINER_DELIMITER ", "
+#define CONTAINER_LEFT_BORDER "["
+#define CONTAINER_RIGHT_BORDER "]"
+#else
+#define CONTAINER_DELIMITER " "
+#define CONTAINER_LEFT_BORDER ""
+#define CONTAINER_RIGHT_BORDER ""
+#endif
+
 using namespace std;
 
 namespace my {
@@ -27,26 +38,34 @@ namespace my {
 
 #define CIN_INIT(type, v) \
     type v;               \
-    cin >> v;
+    cin >> v
 
 #define CIN_INIT_S2(type, v1, v2) \
     type v1, v2;                  \
-    cin >> v1 >> v2;
+    cin >> v1 >> v2
 
 #define CIN_INIT_D2(type1, v1, type2, v2) \
     type1 v1;                             \
     type2 v2;                             \
-    cin >> v1 >> v2;
+    cin >> v1 >> v2
 
 #define CIN_INIT_S3(type, v1, v2, v3) \
     type v1, v2, v3;                  \
-    cin >> v1 >> v2 >> v3;
+    cin >> v1 >> v2 >> v3
+
+#define CIN_INIT_S4(type, v1, v2, v3, v4) \
+    type v1, v2, v3, v4;                  \
+    cin >> v1 >> v2 >> v3 >> v4
 
 #define CIN_INIT_D3(type1, v1, type2, v2, type3, v3) \
     type1 v1;                                        \
     type2 v2;                                        \
     type3 v3;                                        \
-    cin >> v1 >> v2 >> v3;
+    cin >> v1 >> v2 >> v3
+
+#define CIN_INIT_S6(type, v1, v2, v3, v4, v5, v6) \
+    type v1, v2, v3, v4, v5, v6;                  \
+    cin >> v1 >> v2 >> v3 >> v4 >> v5 >> v6
 
     typedef unsigned int uint;
     typedef long long ll;
@@ -55,19 +74,51 @@ namespace my {
     template<typename T>
     string to_string(const vector<T>& input) {
         std::stringstream ss;
-        ss << "[";
+        ss << CONTAINER_LEFT_BORDER;
         if (input.size() > 0) {
-            for (auto it = input.cbegin(); it < --input.cend(); ++it)
-                ss << *it << ", ";
+            for (auto it = input.cbegin(); it != --input.cend(); ++it)
+                ss << *it << CONTAINER_DELIMITER;
             ss << *--input.end();
         }
-        ss << "]";
+        ss << CONTAINER_RIGHT_BORDER;
+        return ss.str();
+    }
+
+    template<typename T>
+    string to_string(const list<T>& input) {
+        std::stringstream ss;
+        ss << CONTAINER_LEFT_BORDER;
+        if (input.size() > 0) {
+            for (auto it = input.cbegin(); it != --input.cend(); ++it)
+                ss << *it << CONTAINER_DELIMITER;
+            ss << *--input.end();
+        }
+        ss << CONTAINER_RIGHT_BORDER;
+        return ss.str();
+    }
+
+    template<typename T1, typename T2>
+    string to_string(const pair<T1, T2>& input) {
+        std::stringstream ss;
+        ss << input.first << CONTAINER_DELIMITER << input.second;
         return ss.str();
     }
 
     template<typename T>
     ostream& operator<<(ostream& os, const vector<T>& input) {
         os << to_string<T>(input);
+        return os;
+    }
+
+    template<typename T>
+    ostream& operator<<(ostream& os, const list<T>& input) {
+        os << to_string<T>(input);
+        return os;
+    }
+
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& input) {
+        os << to_string<T1, T2>(input);
         return os;
     }
 
@@ -98,9 +149,15 @@ namespace my {
 
 using namespace my;
 
+void solution_X() {
+
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    solution_X();
 
     return 0;
 }
